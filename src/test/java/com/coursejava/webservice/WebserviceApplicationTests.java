011e4a22b77da115c4df1d259da301bed5172242
Mateus Lotufo <mateus-lotufo@hotmail.com>
2026-01-18 19:39:27 -0300

    Project created

---
+++ src/test/java/com/coursejava/webservice/WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursejava.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
