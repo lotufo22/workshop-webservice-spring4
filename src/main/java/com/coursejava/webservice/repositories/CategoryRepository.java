b1ec924e1201243cf3a8da647ee1fbc54978b5a3
Mateus Lotufo <mateus-lotufo@hotmail.com>
2026-01-19 20:44:20 -0300

    Category

---
+++ src/main/java/com/coursejava/webservice/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.coursejava.webservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.coursejava.webservice.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
