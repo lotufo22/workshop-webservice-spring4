3d8127598c392c2baf6c891cbfb73fff14bd233e
Mateus Lotufo <mateus-lotufo@hotmail.com>
2026-01-21 21:00:42 -0300

    Payment, one-to-one association

--- src/main/java/com/coursejava/webservice/config/TestConfig.java
+++ src/main/java/com/coursejava/webservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.coursejava.webservice.entities.Category;
 import com.coursejava.webservice.entities.Order;
 import com.coursejava.webservice.entities.OrderItem;
+import com.coursejava.webservice.entities.Payment;
 import com.coursejava.webservice.entities.Product;
 import com.coursejava.webservice.entities.User;
 import com.coursejava.webservice.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 
